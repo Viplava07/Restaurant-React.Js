ac81c2701564f0b03b50824014b0cf96620e7bd3
Viplava Bhardwaj <viplavabhardwaj007@gmail.com>
2020-05-05 14:35:07 +0530

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();    
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
   render() {
@@ -113,7 +113,7 @@ class CommentForm extends Component {
 }
 
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
         if (comments != null)         
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -130,7 +130,7 @@ function RenderComments({comments, addComment, dishId}) {
                               );
                        })}       
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment}/>
+                    <CommentForm dishId={dishId} postComment={postComment}/>
                  </div>
           );
      else 
@@ -191,7 +191,7 @@ const Dishdetail = (props) => {
                     <RenderDish dish={props.dish} />
                     <RenderComments 
                        comments={props.comments}
-                       addComment={props.addComment}
+                       postComment={props.postComment}
                        dishId={props.dish.id}
                     />
                 </div>
